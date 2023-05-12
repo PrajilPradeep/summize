20bfeaa50f1af672ba68dff0963a6d15b8222de5
Prajil <Prajil7425@gmail.com>
2023-05-12 10:57:07 +0530

    Change logo

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -3,7 +3,7 @@ function PageHeader() {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="summize_logo" className="w-28 object-contain" />
+        <img src={logo} alt="summize_logo" className="w-44 object-contain" />
         <button
           type="button"
           onClick={() => {
